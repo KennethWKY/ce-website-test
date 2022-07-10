1d92c8423107d3f44f4951029f4429d526a00a51
KennethWKY <kennethwong630@gmail.com>
2022-07-09 20:50:31 -0400

    Jul 9

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -13,18 +13,6 @@ import {
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBK61hlEibmu-ihjqLp5DtyTrEiwfWmtj0",
-//   authDomain: "ce-website-test.firebaseapp.com",
-//   projectId: "ce-website-test",
-//   storageBucket: "ce-website-test.appspot.com",
-//   messagingSenderId: "661045100823",
-//   appId: "1:661045100823:web:e3e08386bd20d3ff8ab752",
-//   measurementId: "G-V8Z0H0LK2V",
-// };
-
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
